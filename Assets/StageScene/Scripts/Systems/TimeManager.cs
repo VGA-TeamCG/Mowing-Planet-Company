50df610b577089a016084e25727e4aab388d55f0
Zukky_13 <www.kase@gmail.com>
2019-01-19 11:20:31 +0900

    タイムリミット管理を行うTimeManager.csを作成. 概要は以下
    時間(minute and seconds)を設定して呼び出し
    StartCountDown()の呼び出しでカウントダウン開始.現状はinspector上でタイムリミットを設定する方式になっているが必要に応じて変更可能にする予定(再利用しやすい様汎用性を高める)
    カウントダウン開始時、カウントダウン中、カウントダウン終了時に各イベントを発行する処理あり
    OnStartCountDown,OnDuringCountDown,OnEndCountDown

--- Assets/StageScene/Scripts/Systems/TimeManager.cs
+++ Assets/StageScene/Scripts/Systems/TimeManager.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 
 namespace MowingPlanetCompany.StageScene
 {
+
     /// <summary>
     /// 時間管理クラス
     /// </summary>
     public class TimeManager : MonoSingleton<TimeManager>
     {
 
-        #region Field
-
+        #region Property
         /// <summary>タイマーのスイッチングフラグ</summary>
         public bool Toggle
         {
@@ -32,46 +32,45 @@ namespace MowingPlanetCompany.StageScene
         }
 
 
+
+        #endregion
+        #region Field
+        /// <summary>イベントに使用するデリゲート</summary>
+        public delegate void CountDownEvent();
+
+        /// <summary>カウントダウン開始時に発行されるイベント</summary>
+        public static event CountDownEvent OnStartCountDown;
+        /// <summary>カウントダウン中毎フレーム発行されるイベント</summary>
+        public static event CountDownEvent OnDuringCountDown;
+        /// <summary>カウントダウン終了時に発行されるイベント</summary>
+        public static event CountDownEvent OnEndCountDown;
+
+
         [Header("Parameters")]
 
         /// <summary>初期化時に代入する分数</summary>
         [SerializeField] int m_setMinute;
         /// <summary>初期化時に代入する秒数</summary>
         [SerializeField] float m_setSeconds;
-        /// <summary>time scale</summary>
-        [Range(0, 5)] [SerializeField] float m_timeScale = 1;
-
 
         [Header("Components")]
 
         StageManager m_stageManager;
         /// <summary>残り時間を表示するテキスト</summary>
-        [SerializeField] Text m_timeLimitText;
+        [SerializeField] Text m_timerText;
 
 
-        /// <summary></summary>
+        /// <summary>残り分数</summary>
         int m_minute;
-        /// <summary></summary>
+        /// <summary>残り秒数</summary>
         float m_seconds;
         /// <summary></summary>
         float m_totalSeconds;
-        /// <summary></summary>
-        float m_oldSeconds;
         /// <summary>タイマーのスイッチングフラグ</summary>
-        private bool m_toggle;
+        bool m_toggle;
 
         #endregion
         #region Method
-        /// <summary>
-        /// 初期化
-        /// </summary>
-        public void Initialize()
-        {
-            m_minute = m_setMinute;
-            m_seconds = m_setSeconds;
-            m_totalSeconds = m_setMinute * 60 + m_setSeconds; // 単位を秒に変換
-            m_oldSeconds = 0f;
-        }
 
         private void Awake()
         {
@@ -80,15 +79,76 @@ namespace MowingPlanetCompany.StageScene
 
         private void Update()
         {
-            if (Time.timeScale != m_timeScale)
+            if (m_toggle)
             {
-                Time.timeScale = m_timeScale;
+                DuringCountDown();
             }
-            if (!m_toggle)
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public void InitTimer()
+        {
+            m_minute = m_setMinute;
+            m_seconds = m_setSeconds;
+            m_totalSeconds = m_setMinute * 60 + m_setSeconds; // 単位を秒に変換
+        }
+
+
+        /// <summary>
+        /// カウントダウン開始
+        /// </summary>
+        public void StartCountDown()
+        {
+            InitTimer();
+            m_toggle = true;
+
+            // =============
+            // カウントダウン開始時のイベントコール : Event Call that "OnStartCountDown()"
+            // =============
+            if (OnStartCountDown != null)
+                OnStartCountDown();
+        }
+
+        /// <summary>
+        /// カウントダウンが行われている間の処理
+        /// </summary>
+        void DuringCountDown()
+        {
+            // =============
+            // カウントダウン中のイベントコール : Event Call that "OnDuringCountDown()"
+            // =============
+            if (OnDuringCountDown != null)
+                OnDuringCountDown();
+
+            //  カウントダウン処理を行う. 残り時間が無くなったら終了しイベントを発行する
+            m_seconds -= Time.deltaTime;
+            if (m_seconds <= 0)
             {
-                return;
+                if (m_minute == 0)
+                {
+                    m_seconds = 0;
+                    DisplayText();
+                    m_toggle = false;
+                    OnEndCountDown();
+                }
+
+                if (m_minute > 0)
+                {
+                    m_minute--;
+                    m_seconds += 60;
+                    DisplayText();
+                }
             }
-            m_totalSeconds  -= Time.deltaTime;
+        }
+
+        /// <summary>
+        /// 画面にテキスト表示を行う
+        /// </summary>
+        void DisplayText()
+        {
+            m_timerText.text = "あと" + m_minute + "分" + (int)m_seconds + "秒";
         }
         #endregion
     }
