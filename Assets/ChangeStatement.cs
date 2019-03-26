e57d5d635638cead32577c0faa3bbda2efbc53a8
ToshihiroTakasaki <aaa.toshi.19960404@gmail.com>
2019-03-27 01:24:23 +0900

    領収書・明細書のUI配置をした。Textをいじれるようにした。スコア管理を作り始めた。

---
+++ Assets/ChangeStatement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// リザルト画面で使うスクリプト
+// 領収書の「明細書を見る」を押せば明細書に
+// 明細書の「戻る」を押せば領収書になる
+
+public class ChangeStatement : MonoBehaviour
+{
+    bool isReceipt = true;
+    GameObject receipt;
+    GameObject detailedStatement;
+
+    void Start()
+    {
+        receipt = GameObject.Find("Receipt");
+        detailedStatement = GameObject.Find("DetailedStatement");
+    }
+
+    public void Change()
+    {
+        if (isReceipt)  // 「明細書を見る」ボタンを押せば、明細書を見るを表示する
+        {
+            isReceipt = false;
+            receipt.GetComponent<Canvas>().enabled = false;
+            detailedStatement.GetComponent<Canvas>().enabled = true;
+        }
+        else  //  「戻る」ボタンを押せば、領収書を表示する
+        {
+            isReceipt = true;
+            receipt.GetComponent<Canvas>().enabled = true;
+            detailedStatement.GetComponent<Canvas>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    /// <summary> 倒したときのポイント </summary>
+    [SerializeField] int m_point = 10;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
