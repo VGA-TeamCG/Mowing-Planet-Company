81d755e3d9260893d7900257f440b208f08b8dbd
Zukky_13 <www.kase@gmail.com>
2019-07-10 09:04:55 +0900

    creating UI of Game start

---
+++ Assets/StageScene/Scripts/UI/StartGameButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MowingPlanetCompany.StageScene
+{
+    public class StartGameButton : MonoBehaviour
+    {
+        void StartGame()
+        {
+            TimeManager.Instance.StartCountDown();
+            gameObject.SetActive(false);
+        }
+    }
+}
