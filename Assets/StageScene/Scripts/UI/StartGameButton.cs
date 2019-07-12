cf8a6e09938f13f7fef42907dc08470051688801
Zukky_13 <www.kase@gmail.com>
2019-07-12 18:30:10 +0900

    creating start count down UI and animation.

--- Assets/StageScene/Scripts/UI/StartGameButton.cs
+++ Assets/StageScene/Scripts/UI/StartGameButton.cs
@@ -6,10 +6,28 @@ namespace MowingPlanetCompany.StageScene
 {
     public class StartGameButton : MonoBehaviour
     {
-        void StartGame()
+        [SerializeField] GameObject gameStartUI;
+        [SerializeField] float animTIme = 1f;
+         [SerializeField] iTween.EaseType easetype;
+
+        public void StartGame()
         {
             TimeManager.Instance.StartCountDown();
-            gameObject.SetActive(false);
+            gameStartUI.SetActive(false);
+        }
+
+        public void VisibleGameStartDisplay()
+        {
+            var hash = new Hashtable()
+            {
+                {"from",0f },
+                {"to",1f },
+                {"time",animTIme },
+                {"easetype",easetype },
+            };
+
+            gameStartUI.SetActive(true);
+            iTween.ValueTo(gameStartUI, hash);
         }
     }
 }
